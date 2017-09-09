b05e2294a6f576cb108804f49efa49ccb4e46b1d
lukas2005 <lukas2005.38@gmail.com>
2017-09-09 10:53:11 +0200

    Got everything working again

---
+++ Assets/CameraScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour {
+
+    public Transform ParentTo;
+    public Camera cam;
+
+    public float NearestClippingBase = 0.11f;
+
+    private float NearestClippingScaled = 0.11f;
+
+    // Use this for initialization
+    void Start() {
+        NearestClippingScaled = transform.parent.localScale.y * NearestClippingBase;
+
+        cam.nearClipPlane = NearestClippingScaled;
+
+        transform.parent = ParentTo;
+    }
+}
---
+++ Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetworkManager : MonoBehaviour {
+
+    public GameObject MainCamera;
+    public Transform[] spawnSpots;
+
+	// Use this for initialization
+	void Start () {
+        PhotonNetwork.ConnectUsingSettings(GameManager.VERSION);
+	}
+
+    void OnJoinedLobby() {
+
+        RoomOptions opts = new RoomOptions();
+        opts.MaxPlayers = 0;
+
+        PhotonNetwork.JoinOrCreateRoom("#", opts, PhotonNetwork.lobby);
+
+    }
+
+    void OnJoinedRoom() {
+
+        Transform spawnSpot = spawnSpots[Random.Range(0, spawnSpots.Length)];
+
+        GameObject myPlayer = PhotonNetwork.Instantiate("Player", spawnSpot.position, spawnSpot.rotation, 0);
+        MainCamera.SetActive(false);
+
+        myPlayer.GetComponent<PlayerContoller>().enabled = true;
+        myPlayer.GetComponent<MyMouseLook>().enabled = true;
+        myPlayer.transform.Find("Camera").gameObject.SetActive(true);
+
+
+        myPlayer.GetComponent<PhotonVoiceSpeaker>().enabled = false;
+        myPlayer.GetComponent<PhotonVoiceRecorder>().enabled = true;
+
+    }
+
+}
