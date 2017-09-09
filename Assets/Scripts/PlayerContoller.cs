d02f16278a7b47990f2c723fc0c1282c2d3ba342
lukas2005 <lukas2005.38@gmail.com>
2017-09-09 02:42:13 +0200

    Needed to re initialize repo thus loosing old history

---
+++ Assets/Scripts/PlayerContoller.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerContoller : MonoBehaviour {
+
+    public Animator anim;
+    public CharacterController cc;
+
+    int inAirHash = Animator.StringToHash("inAir");
+    int vSpeedHash = Animator.StringToHash("VSpeed");
+    int hSpeedHash = Animator.StringToHash("HSpeed");
+
+    Vector3 vel;
+    public float jumpSpeed = 20f;
+
+    // Update is called once per frame
+    void Update () {
+        float vmove = Input.GetAxis("Vertical") * 1.5f;
+        float hmove = Input.GetAxis("Horizontal") * 1.5f;
+        anim.SetFloat(vSpeedHash, vmove);
+        anim.SetFloat(hSpeedHash, hmove);
+        vel = transform.rotation * new Vector3(hmove, 0, vmove);
+
+        if (cc.isGrounded && !anim.applyRootMotion)
+        {
+            anim.applyRootMotion = true;
+            anim.SetBool(inAirHash, false);
+            vel.y = -1;
+        }
+
+        if (cc.isGrounded && Input.GetButtonDown("Jump"))
+        {
+            anim.applyRootMotion = false;
+            anim.SetBool(inAirHash, true);
+            vel += transform.rotation * new Vector3(0, jumpSpeed, 0);
+        }
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            anim.SetFloat(vSpeedHash, vmove*10);
+        }
+
+        
+    }
+
+    void FixedUpdate() {
+        vel.y += Physics.gravity.y * 2 * Time.deltaTime;
+        if (!anim.applyRootMotion) {
+            cc.Move(vel * Time.deltaTime);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,12 @@
+
+# #VRHackspace
+#VRHackspace Place in VR where hackers and tinkerers can meet! Anyone can contribute!
+# Assets i use (only those that are not in Unity Store are listed here)
+1. General textures from http://devassets.com/assets/general-textures/
+# How to help
+There are two ways of helping :)
+## 1. Make a dontaion
+This is a simplest way of helping currently you can send me some bitcoins to addres provided here (i linked to external webiste because here you can be sure its up to date): https://keybase.io/lukas2005 (Just look for a bitcoin icon on the list)
+Later i will add other ways of donating
+## 2. Directly contribute by coding designing and making pull requests
+Yes just directly start creating stuff you can submit single scenes, scripts, prefabs, textures, models, meshes, make fixes in old ones, make existing scense bigger and better just remember to keep old stuff working :)
