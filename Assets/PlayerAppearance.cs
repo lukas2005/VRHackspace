7335ea522bc413fff16eb3a6a69f2c10ec3cfe6c
lukas2005 <lukas2005.38@gmail.com>
2017-12-07 15:56:56 +0100

    blendshapes are now succesfuly syncing

--- Assets/PlayerAppearance.cs
+++ Assets/PlayerAppearance.cs
@@ -46,26 +46,38 @@ public class PlayerAppearance : Photon.MonoBehaviour {
             stream.SendNext(update);
             if (update) {
                 stream.SendNext(ch.gender);
-                stream.SendNext(ch.clothes.ToArray().Length);
-                foreach (ContentPack cp in ch.clothes) {
+               // stream.SendNext(ch.clothes.ToArray().Length);
+               // foreach (ContentPack cp in ch.clothes) {
                     // Here send
+               // }
+                stream.SendNext(ch.blendshapes.Length);
+                foreach (Morph m in ch.blendshapes)
+                {
+                    stream.SendNext(m.localName);
+                    stream.SendNext(m.value);
                 }
-                stream.SendNext(ch.blendshapes);
             }
         }
         else
         {
-            update = (bool)stream.ReceiveNext();
+            bool updateTmp = (bool)stream.ReceiveNext();
             if (ch == null) ch = ScriptableObject.CreateInstance<Character>();
-            if (update) {
+            if (updateTmp) {
                 ch.gender = (Gender)stream.ReceiveNext();
-                ch.clothes = new List<ContentPack>((int)stream.ReceiveNext());
-                foreach (ContentPack cp in ch.clothes)
-                {
+               // ch.clothes = new List<ContentPack>((int)stream.ReceiveNext());
+               // for (int i = 0; i < ch.clothes.ToArray().Length; i++)
+               // {
                     // Here receive
+                //}
+                ch.blendshapes = new Morph[(int)stream.ReceiveNext()];
+                for (int i = 0; i < ch.blendshapes.Length; i++)
+                {
+                    ch.blendshapes[i] = new Morph();
+                    ch.blendshapes[i].localName = (string)stream.ReceiveNext();
+                    ch.blendshapes[i].value = (float)stream.ReceiveNext();
                 }
-                ch.blendshapes = (Morph[])stream.ReceiveNext();
             }
+            update = updateTmp;
         }
     }
 
