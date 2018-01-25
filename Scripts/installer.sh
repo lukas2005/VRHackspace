cb45e39546a22c08d1b5aa483b366bd820b67c85
lukas2005 <lukas2005.38@gmail.com>
2018-01-25 15:08:34 +0100

    Travis CI setup

---
+++ Scripts/installer.sh
@@ -0,0 +1,18 @@
+#! /bin/sh
+
+# Example install script for Unity3D project. See the entire example: https://github.com/JonathanPorta/ci-build
+
+# This link changes from time to time. I haven't found a reliable hosted installer package for doing regular
+# installs like this. You will probably need to grab a current link from: http://unity3d.com/get-unity/download/archive
+
+cat=$(cat ../ProjectSettings/ProjectVersion.txt)
+version=${cat:17:1000}
+echo $version
+
+url="https://netstorage.unity3d.com/unity/a9f86dcd79df/MacEditorInstaller/Unity-$version.pkg"
+
+echo "Downloading from $url"
+curl -o Unity.pkg $url
+
+echo "Installing Unity.pkg"
+sudo installer -dumplog -package Unity.pkg -target /
