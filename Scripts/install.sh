ca8873708bb1013bd38fabfc1808a37c7d27269e
lukas2005 <lukas2005.38@gmail.com>
2018-01-26 07:46:44 +0100

    fix for travis-ci

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -5,17 +5,13 @@
 # This link changes from time to time. I haven't found a reliable hosted installer package for doing regular
 # installs like this. You will probably need to grab a current link from: http://unity3d.com/get-unity/download/archive
 
-echo "$(pwd)"
-echo "$(ls)"
-
-cat=$(cat ../ProjectSettings/ProjectVersion.txt)
-version=${cat:17:1000}
-echo $version
+version=$(cat ./ProjectSettings/ProjectVersion.txt | cut -d' ' -f 2)
+echo "Using unity version $version"
 
 url="https://netstorage.unity3d.com/unity/a9f86dcd79df/MacEditorInstaller/Unity-$version.pkg"
 
 echo "Downloading from $url"
-curl -o Unity.pkg $url
+curl -o "Scripts/Unity.pkg" $url
 
 echo "Installing Unity.pkg"
-sudo installer -dumplog -package Unity.pkg -target /
+sudo installer -dumplog -package "Scripts/Unity.pkg" -target /
