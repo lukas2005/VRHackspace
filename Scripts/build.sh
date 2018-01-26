577dc977078203853bc6faef1780c47cc45186c1
lukas2005 <lukas2005.38@gmail.com>
2018-01-26 09:24:22 +0100

    another try to fix this ci...

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -5,37 +5,26 @@
 # Change this the name of your project. This will be the name of the final executables as well.
 project="VRHackspace"
 
-time="20"
-
 echo "Attempting to build $project for Windows"
-travis_wait $time /Applications/Unity/Unity.app/Contents/MacOS/Unity \
+/Applications/Unity/Unity.app/Contents/MacOS/Unity \
   -batchmode \
   -nographics \
-  -silent-crashes \
-  -logFile $(pwd)/unity.log \
   -projectPath $(pwd) \
   -buildWindowsPlayer "$(pwd)/Build/windows/$project.exe" \
   -quit
 
 echo "Attempting to build $project for OS X"
-travis_wait $time /Applications/Unity/Unity.app/Contents/MacOS/Unity \
+/Applications/Unity/Unity.app/Contents/MacOS/Unity \
   -batchmode \
   -nographics \
-  -silent-crashes \
-  -logFile $(pwd)/unity.log \
   -projectPath $(pwd) \
   -buildOSXUniversalPlayer "$(pwd)/Build/osx/$project.app" \
   -quit
 
 echo "Attempting to build $project for Linux"
-travis_wait $time /Applications/Unity/Unity.app/Contents/MacOS/Unity \
+/Applications/Unity/Unity.app/Contents/MacOS/Unity \
   -batchmode \
   -nographics \
-  -silent-crashes \
-  -logFile $(pwd)/unity.log \
   -projectPath $(pwd) \
   -buildLinuxUniversalPlayer "$(pwd)/Build/linux/$project.exe" \
   -quit
-
-echo 'Logs from build'
-cat $(pwd)/unity.log
